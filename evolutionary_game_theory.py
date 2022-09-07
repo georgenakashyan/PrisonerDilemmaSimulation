d7b17bd1b71f46db30fd270139e2d6e7398f3d09
GeorgeN <91430969+Gevoooo@users.noreply.github.com>
2022-09-06 20:59:47 -0400

    Add files via upload

---
+++ evolutionary_game_theory.py
@@ -0,0 +1,152 @@
+"""
+This module contains functions to run a simulation of an evolutionary game theory process following
+a Fermi rule approximation strategy on a given network.
+"""
+
+import networkx as nx
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import scipy as sp
+from collections import deque
+
+def _count_coop(strategies):
+    """
+    Counts the portion of nodes with cooperative strategy in the network
+    Parameters
+    ----------
+    strategies : dict
+        A dict containing the strategy of each node
+    Returns
+    -------
+    proportion_coop : float
+        Proportion of nodes using a cooperative strategy
+    """
+    length = len(strategies)
+    return (length - sum(strategies.values())) / length
+
+
+def _fermi_updating_rule(wi, wj, beta):
+    """
+    Returns the probability of individual i adopting individual j strategy
+    Parameters
+    ----------
+    wi : float
+        Payoff of individual i
+    wj : float
+        Payoff of individual j
+    beta : float
+        Parameter that models the importance of a payoff difference
+    Returns
+    -------
+    pij : float
+        Probability of individual i adopting individual j strategy
+    """
+    return 1 / (1 + np.e ** (-beta * (wj - wi)))
+
+
+def one_replica_simulation(G, W, steps, x0, beta, stationary=0.9):
+    """
+    Runs one replica simulation of the evolutionary game theory simulation
+    Parameters
+    ----------
+    G : nx.Graph
+    W : array
+        Payoff matrix
+    steps : int
+        Number of epochs to run
+    x0 : float
+        Proportion of initial nodes using cooperative strategy
+    beta : float
+        Parameter that models the importance of the difference in Fermi updating rule
+    stationary : float
+        Proportion of epochs that correspond to the stationary phase
+    Returns
+    -------
+    p : float
+        mean proportion of nodes using cooperative strategy in the stationary phase
+    time_series : deque
+        time series of the proportion of nodes using cooperative strategy in each time step
+    """
+    time_series = deque()
+    strategy = dict(zip(G.nodes(), np.random.choice([0, 1], len(G.nodes()), p=[x0, 1-x0])))
+
+    for t in range(steps):
+        if t > steps * stationary:
+            time_series.append(_count_coop(strategy))
+        new_strategy = dict()
+        payoffs = _compute_all_payoffs(G, W, strategy)
+        for i in G.nodes():
+            j = random.sample(list(G.neighbors(i)), 1)[0]  # random selected neighbor
+            wi, wj = payoffs.get(i), payoffs.get(j)  # payoffs of each node
+            pij = _fermi_updating_rule(wi, wj, beta)  # probability of node i to adopt j strategy
+            if np.random.random() < pij:
+                new_strategy[i] = strategy.get(j)
+        strategy.update(new_strategy)  # update strategies
+    p = np.mean(time_series)
+    return p, time_series
+
+
+def multi_replica_simulation(G, W, steps, x0, beta, replicas):
+    """
+    Runs one a given number of  simulation replicas of the evolutionary game theory simulation
+    Parameters
+    ----------
+    G : nx.Graph
+    W : array
+        Payoff matrix
+    steps : int
+        Number of epochs to run
+    x0 : float
+        Proportion of initial nodes using cooperative strategy
+    beta : float
+        Parameter that models the importance of the difference in Fermi updating rule
+    replicas : int
+        Number of replicas to execute
+    Returns
+    -------
+    p_mean : float
+        Mean proportion of nodes following a cooperative strategy
+    """
+    return np.mean([one_replica_simulation(G, W, steps, x0, beta)[0] for _ in range(replicas)])
+
+
+def _compute_all_payoffs(G, W, strategy):
+    """
+    Computes the payoffs of each node in the network in a timestep
+    Parameters
+    ----------
+    G : nx.Graph
+    W : array
+        Matrix payoff
+    strategy : dict
+        Dict of strategies followed by each node
+    Returns
+    -------
+    payoffs : dict
+        Payoffs of the nodes in the network
+    """
+    payoffs = {node: _get_payoff(G, node, W, strategy) for node in G.nodes}
+    return payoffs
+
+
+def _get_payoff(G, i, W, strategy):
+    """
+    Gets the payoff of one sample. To obtain the payoff of each node a game between each node and its neighbors is
+    simulated knowing each neighbor strategy. One obtained every result of each game is added.
+    Parameters
+    ----------
+    G : nx.Graph
+    i : int, tuple, str
+        The node of interest to obtain its payoff
+    W : array
+        Matrix payoff
+    strategy : dict
+        Dict of strategies followed by each node
+    Returns
+    -------
+    payoff : float
+        Payoff of the given node
+    """
+    payoff = sum([W[strategy.get(i)][strategy.get(j)] for j in G.neighbors(i)])
+    return payoff
---
+++ main.py
@@ -0,0 +1,47 @@
+import networkx as nx
+import os as os
+import matplotlib.pyplot as plt
+import scipy as sp
+from evolutionary_game_theory import *
+from time_series_plots import *
+from density_plots import *
+
+#Input variables
+os.system('clear')
+print("[=================== Prisoner's Dilemma Game =======================]")
+exitPrompt = False
+while exitPrompt == False:
+    graphChoice = input("\nWhich graph would you like to simulate? (ws for Watts-Strogatz or fb for Facebook): ")
+    if (graphChoice == "ws"):
+        print("\nNote: Default values for these questions are: 5000, 4, 0.1, 10, 25, 0.5 in order.")
+        nodes = int(input("\nNumber of players/nodes (positive integer): "))
+        avgEdgePerNode = int(input("\nNumber of average edges per node (positive integer): "))
+        rewireChance = float(input("\nChance of rewiring (float between 0 and 1 inclusive): "))
+        g = nx.watts_strogatz_graph(nodes, avgEdgePerNode, rewireChance)
+        exitPrompt = True
+    elif (graphChoice == "fb"):
+        print("\nNote: Default values for these questions are: 0.1, 10, 25, 0.5 in order.")
+        rewireChance = float(input("\nChance of rewiring (float between 0 and 1 inclusive): "))
+        path = os.path.split(os.path.realpath(__file__))
+        g = nx.read_edgelist(os.path.normpath(path[0] + "/facebook_combined.txt.gz"), create_using = nx.Graph(), nodetype = int)
+        exitPrompt = True
+    else:
+        print("\nInvalid choice, type fb for Facebook network or ws for Watts-Strogatz network.")
+
+games = int(input("\nNumber of games to simulate (positive integer): "))
+turns = int(input("\nNumber of turns to run each game (positve integer): "))
+initCoop = float(input("\nInitial proportion of cooperators to defectors (float between 0 and 1 inclusive): "))
+
+#Payoff matrix
+payoff = [[0, 1.8], [-0.3, 1.5]]
+
+#Running the simulation and making time series plot
+pDict = plot_time_series(g, payoff, turns, initCoop, rewireChance, games, "Proportion of Cooperators per Time-Step")
+p = sum(pDict.values()) / len(pDict)
+
+#Output of result data
+print("\n[============================= Results =============================]")
+print("\nMean proportion of cooperators after", turns, "turns for", games, "games:", p)
+
+#Video of evolution
+make_simulation_video(g, payoff, turns, initCoop, rewireChance, "Prisoner's Dilemma", 5)
