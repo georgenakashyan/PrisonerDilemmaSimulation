dbfb8c4c35e8a9c2764b4d5f75bd8d728f07561f
George N - PC <georgenakashyan@gmail.com>
2023-01-19 01:20:12 -0500

    Removed output into terminal and instead moved it to title of chart. Also added more comments for payoff matrix.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import networkx as nx
 import os as os
-import matplotlib.pyplot as plt
-import scipy as sp
 from evolutionary_game_theory import *
 from time_series_plots import *
 from density_plots import *
 
 # input variables
-os.system('clear')
+os.system('cls' if os.name == 'nt' else 'clear')
 print("[=================== Prisoner's Dilemma Game =======================]")
 while True:
     graphChoice = input("\nWhich graph would you like to simulate? (ws for Watts-Strogatz or fb for Facebook): ")
@@ -15,7 +13,7 @@ while True:
         print("\nNote: Default values for these questions are: 5000 Nodes, 4 Edges Per Node (on average), 10 games, 25 turns, 0.5 Cooperators.")
         nodes = int(input("\nNumber of players/nodes (positive integer): "))
         avgEdgePerNode = int(input("\nNumber of average edges per node (positive integer): "))
-        g = nx.watts_strogatz_graph(nodes, avgEdgePerNode, 0)
+        g = nx.watts_strogatz_graph(nodes, avgEdgePerNode, 0.5)
         break
     elif (graphChoice == "fb"):
         print("\nNote: Default values for these questions are: 10 Games, 25 Turns, 0.5 Cooperators.")
@@ -30,6 +28,7 @@ turns = int(input("\nNumber of turns to run each game (positve integer): "))
 init_coop = float(input("\nInitial proportion of cooperators to defectors (float between 0 and 1 inclusive): "))
 
 # how players decide to change their strategy
+beta = 0
 while True:
     choice_factor = int(input("""\nWhat will players base their decision to change their strategy on? (Enter the choice number)
                             \n(1) Payoff difference
@@ -42,18 +41,15 @@ while True:
         break
 
 # payoff matrix
-# !: Payoff matrix based off of benefits and costs for the prisoner's dilemma game
-# !: D is defector, C is cooperator. Left is 'i'or the current node, right is 'j' or neighbor node.
-# !: [D:D, D:C], [C:D, C:C]
+# b = benefit given by cooperators, c = cost cooperators bear for giving out b
+# D is defector, C is cooperator. Left is 'i'or the current node, right is 'j' or neighbor node.
+# Assignment of values: [D:D, D:C], [C:D, C:C]
+# Values dictated by:   [0, b], [-c, b-c]
 payoff = [[0, 1.8], [-0.3, 1.5]]
 
 # running the simulation and making time series plot
 pDict = plot_time_series(g, payoff, turns, init_coop, beta, games, choice_factor, "Proportion of Cooperators per Time-Step")
 p = sum(pDict.values()) / len(pDict)
-
-# output of result data
-print("\n[============================= Results =============================]")
-print("\nMean proportion of cooperators after", turns, "turns for", games, "games: ", p)
 
 # video of evolution
 make_simulation_video(g, payoff, turns, init_coop, beta, choice_factor, "Prisoner's Dilemma", 5)
