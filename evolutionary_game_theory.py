a3f4aa2ba482d41c1463f0b3d04e830f3c3e211e
George N - PC <georgenakashyan@gmail.com>
2023-01-19 21:34:11 -0500

    Removed unnecessary imports

--- evolutionary_game_theory.py
+++ evolutionary_game_theory.py
@@ -3,10 +3,7 @@ This module contains functions to run a simulation of an evolutionary game theor
 a Fermi rule approximation strategy on a given network.
 """
 
-#import networkx as nx
 import numpy as np
-#import matplotlib.pyplot as plt
-#import scipy as sp
 import logging
 from collections import deque
 
